8c6bbc8ce76b0203a66fd629ad6561d4e41540f4
Румен Драганов <r.draganov@respect93.com>
2015-09-18 23:42:01 +0300

    Тестове

--- MyEc2/Interact.cs
+++ MyEc2/Interact.cs
@@ -9,44 +9,48 @@ namespace MyEc2
 	public class Interact
 	{
 		//private double delt1 = 0, delt2=0;
-		private double eps = 0, epc = 0, x=0, pts=6;
+		private double eps = 0, epc = 0, x=0, pts=16;
 		private enum nx{ix=0,iec,ies,iM,iN};
 		private string[] dp1;
 		private List<string[]> l1 =new List<string[]>();
 		SBsec _sbs1;
 		private double F1 = 0, M1 = 0, F2 = 0, M2 = 0;
 		private double h;
+		double maxM=0, minM=0;
 
 		public Interact (SBsec sbs1)
 		{
 			_sbs1 = sbs1;
-			double epsmax=_sbs1.armList [1]._st.eps_uk * 0.9;
-			double epsud=_sbs1.armList [1]._st.f_yk/sbs1.armList [1]._st.Es/1.15;
+			double epsud=_sbs1.armList [1]._st.eps_uk * 0.9;
+			double epssd=_sbs1.armList [1]._st.f_yk/sbs1.armList [1]._st.Es/1.15*1000;
 			double epsmin = _sbs1.abet.eps_cu3;
 			double epsc2 = _sbs1.abet.eps_c2;
-			eps = epsmax;
-			epc = epsmax;
+			Console.WriteLine ("epsud = " + epsud + "   epssd = " + epssd);
+			eps = epsud;
+			epc = epsud;
 			h = _sbs1.maxY - _sbs1.minY;
 
-			FLoop (-epsmax / pts,0);   						//долу опън на мах, горе опън от мах до нула
+			FLoop (-epsud / pts,0);   						//долу опън на мах, горе опън от мах до нула
 			FLoop (epsmin / pts,0); 						//долу опън на мах, горе натиск от нула до мах
-			pts = 16;
-			FLoop (0,- (epsmax) / pts);
-			pts = 6;
-			//FLoop (0,- (epsmax-epsud) / pts); 				// долу опън от мах до нула, горе ръб натиск на мах
+			pts=64;
+			FLoop (0,- (epsud) / pts);
+
+			//FLoop (0,- (epssd) / pts); 				// долу опън от мах до нула, горе ръб натиск на мах
+			pts=16;
 			FLoop (-(epc - epsc2) / pts,(eps +epsc2) / pts);// долу натиск от нула до 2, горе от мах до 2
 			FLoop (-epc / pts, -(eps-epsmin) / pts); 		// горе натиск от 2 до нула, долу натиск от 2 до мах
-			pts = 32;
-			FLoop (epsmax / pts,0); 						// горе се опъва от нула до мах, долу натиск на мах
-			pts = 6;
-			FLoop (0,-eps/pts); 							// горе опън на мах, долу натиск до нула
-			FLoop (0,epsmax / pts);							// горе опън на мах, долу расте до опън на мах
+			pts=64;
+			FLoop (epsud / pts,0); 						// горе се опъва от нула до мах, долу натиск на мах
+			pts=16;
+			FLoop (0,-epsmin/pts); 							// горе опън на мах, долу натиск до нула
+			FLoop (0,epsud / pts);							// горе опън на мах, долу расте от нула до опън на мах
 			if (savea ()) Console.WriteLine("Запис - готово");
+			Console.WriteLine("maxM = "+maxM+"  minM = "+minM);
 		}
 
 		public void FLoop(double d1, double d2)
 		{
-			for (int i = 0; i < pts; i++, epc+=d1,eps+=d2) {
+			for (int i = 0; i < pts+1; i++, epc+=d1,eps+=d2) {
 				_sbs1.AnalyseStress (epc, eps, _sbs1.ddSec,Bet.bsx.bsx2,out F1,out M1);
 				_sbs1.AnalyseStressArm (epc, eps, Stom.ssx.ssx1, out F2,out M2);
 				if (eps == epc)
@@ -62,9 +66,16 @@ namespace MyEc2
 				dp1 [(int)nx.iN] =(-0.1*(F1+F2)).ToString("F2");
 				l1.Add (dp1);
 
-				//Console.WriteLine(x.ToString("N2").PadRight(15)+(M1+M2).ToString("N2").PadRight(15)+
-				//				 (F1+F2).ToString("N2").PadRight(12));
+				if (maxM < M1 + M2)
+					maxM = M1 + M2;
+				if (minM > M1 + M2)
+					minM = M1 + M2;
+
+
 			}
+			epc -= d1;eps -= d2;
+			string[] dp0=new string[]{"","","","0","0"};
+			l1.Add (dp0);
 			//Console.WriteLine ("epc = " + epc.ToString ("N3") + "   eps = " + eps.ToString ("N3"));
 
 		}
